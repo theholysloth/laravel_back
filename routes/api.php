975b4b7ade6ec18fcc8ee8d6d2e46d1fa72eeea2
theholysloth <82107414+theholysloth@users.noreply.github.com>
2026-03-25 17:36:08 +0100

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
+//use App\Http\Controllers\Api\TokenLogoutController;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TaskController; 
+use Illuminate\Http\Request;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']); //ces routes sont accessibles sans etre connecté
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('tasks', TaskController::class); 
+//Route::post('/logout', [AuthController::class, 'logout']);
+//Route::apiResource('tasks', TaskController::class);
+
+Route::middleware('auth:sanctum')->get('/me',function (Request $request){
+    return response()->json([
+        'user '=> $request->user(),
+    ]);
+});
+
+Route::middleware('auth:sanctum')->group(function () {//pour acceder à ces routes, il faut etre authentifié par sanctum
+    //Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('tasks', TaskController::class);
+
+
+});
