99f594a08b90d6ccea7daed1004e53356377a5d8
theholysloth <82107414+theholysloth@users.noreply.github.com>
2026-03-25 10:00:25 +0100

    Add files via upload

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests\LoginUserRequest; 
+use App\Http\Requests\RegisterUserRequest; 
+use Illuminate\Support\Facades\Hash;
+
+
+
+class AuthController extends Controller
+{
+    /*
+    Fortify installe automatiquement
+        -  Les routes /login, /register, /logout grace à php artisan vendor:publish --provider="Laravel\Fortify\FortifyServiceProvider"
+        - les actions d'authentification dans le dossier Actions
+        
+        
+        
+        
+        */
+
+
+
+    public function login(LoginUserRequest $request){
+        $validated = $request->validated(); //on recupere les données validées par le form request  Fortify::authenticateUsing(...)
+
+        $user = User::where('email', $validated['email'])->first();
+        if (!$user || !Hash::check($validated['password'], $user->password)){
+            return response()->json(['message' => ' informations erronées'], 401); 
+        }
+
+        $token = $user->createToken('firstToken')->plainTextToken; //generation d'un Api token avec sanctum
+
+        return response()->json([
+            'message' => 'Connexion reussie ',
+            'user' => $user, 
+            'token' => $token
+        ]);
+    }
+
+    public function register(RegisterUserRequest $request){
+        $validated = $request->validated();
+        dd('register hit');
+        $user = User::create([
+            'name' =>$validated['name'], 
+            'email' =>$validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken('firstToken')->plainTextToken; 
+
+        return response()->json([
+            'message' => 'Utilisateur enregistré!',
+            'user' => $user, 
+            'token' => $token
+        ],201);
+    }
+
+    public function loggout( Request $request){
+        $request->user()->currentAccessToken()->delete();
+        
+        return response()->json([
+            'message' => 'deconnexion reussie!',
+        ]);
+    }
+
+    public function me(Request $request){
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    }
+    
+}
