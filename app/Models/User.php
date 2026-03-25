1992d3da491e2fe3e5933bcbdcda20da4ec841a5
theholysloth <82107414+theholysloth@users.noreply.github.com>
2026-03-25 17:30:19 +0100

    Add files via upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,24 +9,25 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; //ce trait est necessaire car il permet l'authentification API et la creation de tokens personnels
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+//#[Fillable(['name', 'email', 'password'])]
+//#[Hidden(['password', 'remember_token'])]
+
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $fillable = [
+        'name',
+        'email', 
+        'password'
+    ] ; 
+
+    protected $hidden = [
+        'password', 
+        'remember_token'
+    ];
+
 }
