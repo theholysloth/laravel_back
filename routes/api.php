f83b83f7e78557a1f114362b00cd46494dd5ea84
theholysloth <82107414+theholysloth@users.noreply.github.com>
2026-04-01 17:51:37 +0200

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserManagementController;
+
 
 
 Route::post('/register', [AuthController::class, 'register']); //ces routes sont accessibles sans etre connecté
@@ -13,16 +15,23 @@ Route::post('/login', [AuthController::class, 'login']);
 //Route::post('/logout', [AuthController::class, 'logout']);
 //Route::apiResource('tasks', TaskController::class);
 
-Route::middleware('auth:sanctum')->get('/me',function (Request $request){
-    return response()->json([
-        'user '=> $request->user(),
-    ]);
-});
 
 Route::middleware('auth:sanctum')->group(function () {//pour acceder à ces routes, il faut etre authentifié par sanctum
-    //Route::get('/me', [AuthController::class, 'me']);
+    Route::get('/me',function (Request $request){
+        return response()->json([
+            'user'=> $request->user()->load('roles'),
+        ]);
+    });
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/users', [UserManagementController::class,'index']);
+    Route::put('/users/{user}/role', [UserManagementController::class,'updateRole']);
+    Route::get('/roles', [UserManagementController::class,'roles']);
+
+    Route::post('/users', [CreateUserByAdminRequest::class,'store']);
+
     Route::apiResource('tasks', TaskController::class);
+
 
 
 });
