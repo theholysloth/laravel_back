fb89aeca06abe831ae5d74564307be73cf260642
theholysloth <82107414+theholysloth@users.noreply.github.com>
2026-04-01 17:48:50 +0200

    Add files via upload

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use App\Models\User;
 
 
 class Task extends Model
@@ -21,4 +22,8 @@ class Task extends Model
         'date', 
         'done'
     ];
+
+    public function user(){//grace à ces realtions on pourra juste faire task->user() pour avoir l'user chargé de ces taches 
+        return $this->belongsTo(Task::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,14 +10,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens; //ce trait est necessaire car il permet l'authentification API et la creation de tokens personnels
+use Spatie\Permission\Traits\HasRoles; 
 
+use App\Models\Task;
 //#[Fillable(['name', 'email', 'password'])]
 //#[Hidden(['password', 'remember_token'])]
 
 class User extends Authenticatable
-{
+{   //admin, user = roles
+    //permissions : view all tasks, delete any task
+    //super-admin : view users, assign roles, assign permissions
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasRoles, HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = [
         'name',
@@ -29,5 +33,9 @@ class User extends Authenticatable
         'password', 
         'remember_token'
     ];
+
+    public function tasks(){//grace à ces realtions on pourra juste faire user->tasks() pour avoir les taches associées à cet user
+        return $this->hasMany(Task::class); 
+    }
 
 }
